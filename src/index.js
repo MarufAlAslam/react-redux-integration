a2d163735c19ba4a2199365348fc1b234b77b671
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-09-17 11:54:59 +0600

    store created and connected with APP

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/css/globals.css'
 import './assets/css/configs.css'
+import { Provider } from 'react-redux';
+import store from './redux/store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
